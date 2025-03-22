bff449697c0a5572543e30b9b0e98418f68372e3
kayveex <rhesakornelius@gmail.com>
2025-03-22 10:57:56 +0700

    feat: first init & section 2-15

---
+++ app/Models/Todos.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todos extends Model
+{
+    protected $table = 'todos';
+    protected $primaryKey = 'id';
+    protected $fillable = ['task', 'is_done'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Todo\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/todos', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
