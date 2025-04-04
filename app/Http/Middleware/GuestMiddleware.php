93563cc5f0be56a1dc158b823a38758839ca9796
unknown <rhesakornelius@gmail.com>
2025-04-04 13:47:18 +0700

    feat: section 2-38 middleware

---
+++ app/Http/Middleware/GuestMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class GuestMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            // Redirect to todos page if already authenticated
+            return redirect()->route('todos')->withErrors('error', 'Anda sudah login, silahkan logout terlebih dahulu!');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware('auth')->group(function() {
+    Route::get('/todos', [TodoController::class, 'index'])->name('todos');
+    Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+    Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todos.update');
+    Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.delete');
+});
 
-Route::get('/todos', [TodoController::class, 'index'])->name('todos');
-Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
-Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todos.update');
-Route::delete('/todos/{id}', [TodoController::class, 'destroy'])->name('todos.delete');
 
-Route::get('/user/login', [UserController::class, 'login'])->name('login');
-Route::post('/user/login', [UserController::class, 'doLogin'])->name('doLogin');
+Route::middleware('guest')->group(function() {
+    Route::get('/user/login', [UserController::class, 'login'])->name('login');
+    Route::post('/user/login', [UserController::class, 'doLogin'])->name('doLogin');
+});
+
+
 Route::post('/user/logout', [UserController::class, 'logout'])->name('logout');
